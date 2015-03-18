fadf8091f4ed5c75e667900f8c1e28fbf58d250e
AwesomeBen1 <AwesomeBen1@users.noreply.github.com>
2015-03-17 21:59:58 -0700

    Gameplay and source code improvements
    Reorganized and re-wrote code to be better, added new outcomes to existing actions, and added down button functionality to repeat last action.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
 // Cold War Simulator 2K15
-// Made by Ben Chapman-Kish up to and on 2015-03-16
+// Made by Ben Chapman-Kish up to and on 2015-03-17
 #include "pebble.h"
 // The possibilities of segfaults, memory overflows, and memory leaks are features, by the way
-// Features to add: up button to see game history, down button to repeat last action
+// Features to add: up button to see game history, ability to restart the game
 
 #define TIMER_MS 3000
 typedef struct {
@@ -16,7 +16,7 @@ ColdWarOption options[] = {
 	{ .action = "Send Spies", .description = "Get that precious intel."},
 	{ .action = "Ask To Not Nuke", .description = "Please don't hurt us!"}
 };
-#define NUM_OPTIONS = sizeof(options) / sizeof(ColdWarOption)
+// define NUM_OPTIONS = sizeof(options) / sizeof(ColdWarOption)
 
 static Window *s_initial_splash, *s_main_window, *s_menu_window, *s_end_window;
 static MenuLayer *s_menu_layer;
@@ -25,29 +25,39 @@ static BitmapLayer *s_splash_layer, *s_background_layer, *s_end_layer;
 static GBitmap *s_splash_bitmap, *s_background_bitmap, *s_end_win_bitmap, *s_end_lose_bitmap;
 static AppTimer *s_timer;
 static bool player_won, end_game = false;
-static int randnum, end_outcome = 0, nonukestreak = 0;
+static int randnum, last_action, turn = 0, end_outcome = 0, nonukestreak = 0;
 static int stats[5] = {1,1,1,1,100}; // Your power, their power, your smarts, their smarts, tensions
 static char s_stats_buffer[128];
 
 
 
+static int randrange(int min_val, int max_val) {
+	// It's like random.randrange in python, but inclusive, and without default parameters!
+	return (rand() % (1 + max_val - min_val)) + min_val;
+}
+
 static void update_stats_text(void) {
 	snprintf(s_stats_buffer, sizeof(s_stats_buffer), "Your Power: %d\nTheir Power: %d\n\
 	Your Smarts: %d\nTheir Smarts: %d\nTensions: %d", stats[0],stats[1],stats[2],stats[3],stats[4]);
+	/* I wish there was a more elegant way to pass all items in the stats array.
+	In python you can pass all items in a list by doing *list in a function,
+	But in C, an asterisk before a variable has something to do with a pointer. */
 	text_layer_set_text(s_stats_layer, s_stats_buffer);
 }
 
 static void timer_show_end(void *data) {
+	/* Timer callbacks: Because psleep didn't work.
+	I would have passed end_outcome as a parameter, but I don't think I can. */
 	switch (end_outcome) {
 		case 1:
 			text_layer_set_text(s_info_layer, "Tensions are so low you both demilitarize!");
 			break;
 		case 2:
-			stats[4] *= 100;
+			stats[4] = (stats[4] + randrange(10, 100)) * randrange(10, 1000) + randrange(0, 100);
 			text_layer_set_text(s_info_layer, "They nuke you and you can't retaliate!");
 			break;
 		case 3:
-			stats[4] *= 100;
+			stats[4] = (stats[4] + randrange(10, 100)) * randrange(10, 1000) + randrange(0, 100);
 			text_layer_set_text(s_info_layer, "They nuke you and you nuke them back.");
 			break;
 	}
@@ -55,39 +65,131 @@ static void timer_show_end(void *data) {
 }
 
 static void post_turn_event(void) {
-	window_stack_remove(s_menu_window, true);
-	update_stats_text();
 	for (int i = 0 ; i < 4 ; i++) {
-		stats[i] += rand() % 10 + 1;
+		stats[i] += randrange(1, 10); // stats increase by natural progression
 		if (stats[i] < 1) {
-			stats[i] = 1;
+			stats[i] = 1; // stats can't be less than one!
 		}
 	}
 	if (end_game) {
 		app_timer_register(TIMER_MS, timer_show_end, NULL);
 	} else {
-		if (stats[4] < 1) {
+		if (stats[4] < 1) { // If tensions are low enough, everybody wins!
 			end_game = true;
 			player_won = true;
 			stats[4] = 0;
 			update_stats_text();
 			end_outcome = 1;
 			app_timer_register(TIMER_MS, timer_show_end, NULL);
-    } else if (stats[4] > 500) {
+    } else if (stats[4] > 500) { // If tensions are high enough, they nuke you anyway!
 			end_game = true;
 			player_won = false;
 			update_stats_text();
-			if (stats[1] + stats[3] > (stats[0] + stats[2]) * 3) {
+			if (stats[1] + stats[3] > (stats[0] + stats[2]) * 3) { // But are you able to retaliate?
 				end_outcome = 2;
-				app_timer_register(TIMER_MS, timer_show_end, NULL);
 			} else {
 				end_outcome = 3;
-				app_timer_register(TIMER_MS, timer_show_end, NULL);
 			}
+			app_timer_register(TIMER_MS, timer_show_end, NULL);
 		} else {
 			update_stats_text();
 		}
 	}
+}
+
+static void take_action(int action) {
+	switch (action) {
+		case 0:
+			// If nukes start getting thrown around, tensions soar	
+			stats[4] = (stats[4] + randrange(10, 100)) * randrange(10, 1000) + randrange(0, 100);
+			end_game=true;
+			if (stats[0] + stats[2] > (stats[1] + stats[3] * 3) + 10) {
+				// Unrealistic for one side to nuke the other and survive, but it's gameplay incentive
+				text_layer_set_text(s_info_layer, "You destroyed them and won the cold war!");
+				player_won=true;
+			} else {
+				// This is why the real cold war ended peacefully.
+				text_layer_set_text(s_info_layer, "You nuke each other! Ever heard of MAD?");
+				player_won=false;
+			}
+			break;
+		case 1:
+			randnum = rand() % 16;
+			// So many proxy wars in the cold war and they all ended in different ways...
+			if (randnum < 2) {
+				text_layer_set_text(s_info_layer, "The conflict ends with an armistice.");
+				stats[4] += randrange(-10, 10);
+			} else if (randnum < 4) {
+				text_layer_set_text(s_info_layer, "The proxy country becomes independant.");
+				stats[4] -= randrange(10, 40);
+			} else if (randnum < 8) {
+				text_layer_set_text(s_info_layer, "You won the proxy war! You gain power!");
+				stats[0] += randrange(10, 80);
+				stats[4] += randrange(10, 40);
+			} else if (randnum < 12) {
+				text_layer_set_text(s_info_layer, "They won the proxy war and gain power.");
+				stats[1] += randrange(10, 80);
+				stats[4] += randrange(10, 40);
+			} else if (randnum < 14) {
+				text_layer_set_text(s_info_layer, "You win the proxy war, but at great cost.");
+				stats[0] += randrange(-10, 10);
+				stats[1] -= randrange(0, 20);
+				stats[4] += randrange(10, 40);
+			} else {
+				text_layer_set_text(s_info_layer, "They achieve a pyrrhic victory.");
+				stats[0] -= randrange(0, 20);
+				stats[1] += randrange(-10, 10);
+				stats[4] += randrange(10, 40);
+			}
+			break;
+		case 2:
+			randnum = rand() % 12;
+			// These outcomes are reminiscent of CIA vs. KGB fiction
+			if (randnum < 4) {
+				text_layer_set_text(s_info_layer, "You gain valuable intel about them.");
+				stats[2] += randrange(10, 80);
+			} else if (randnum < 8) {
+				text_layer_set_text(s_info_layer, "Your spies are caught and they are angry.");
+				stats[4] += randrange(30, 100);
+			} else if (randnum < 10) {
+				text_layer_set_text(s_info_layer, "Your spies kill some of their spies.");
+				stats[3] -= randrange(5, 25);
+				stats[4] += randrange(30, 100);
+			} else {
+				// Sort of like what happened with Igor Gouzenko... kind of
+				text_layer_set_text(s_info_layer, "Your spies defect and tell them secrets.");
+				stats[3] += randrange(10, 80);
+				stats[4] += randrange(10, 40);
+			}
+			break;
+		case 3:
+			randnum = rand() % 12;
+			if (stats[4] > 300 || randnum > 8 || nonukestreak >= 4) {
+				text_layer_set_text(s_info_layer, "Your plea incites them to make more nukes.");
+				stats[1] += randrange(5, 20);
+				stats[4] += randrange(10, 30);
+			} else if (stats[4] > 150 || randnum > 6 || (nonukestreak >= 2 && randnum > 4)) {
+				text_layer_set_text(s_info_layer, "They politefully decline your request.");
+				stats[4] += randrange(-5, 5);
+			} else if (randnum > 2) {
+				// It's just like the October Crisis!
+				text_layer_set_text(s_info_layer, "You both agree to cut down on the nukes.");
+				stats[0] -= randrange(5, 20);
+				stats[1] -= randrange(5, 20);
+				stats[4] -= randrange(20, 70);
+			} else {
+				text_layer_set_text(s_info_layer, "They remove some nukes close to you.");
+				stats[1] -= randrange(5, 20);
+				stats[4] -= randrange(10, 40);
+			}
+			break;
+	}
+	if (action == 3) {
+		nonukestreak++;
+	} else {
+		nonukestreak = 0;
+	}
+	turn++;
 }
 
 
@@ -111,75 +213,16 @@ static void menu_draw_row(GContext* ctx, const Layer *cell_layer, MenuIndex *cel
 	menu_cell_basic_draw(ctx, cell_layer, option->action, option->description, NULL);
 }
 
+
+
 static void menu_select(MenuLayer *menu_layer, MenuIndex *cell_index, void *data) {
   // Use the row to specify which item will receive the select action
-	switch (cell_index->row) {
-		case 0:
-			stats[4] = (stats[4] + (rand() % 90 + 10)) * (rand() % 990 + 10) + (rand() % 101);
-			end_game=true;
-			if (stats[0] + stats[2] > (stats[1] + stats[3] * 3) + 10) {
-				text_layer_set_text(s_info_layer, "You destroyed them and won the cold war!");
-				player_won=true;
-			} else {
-				text_layer_set_text(s_info_layer, "You nuke each other! Ever heard of MAD?");
-				player_won=false;
-			}
-			nonukestreak = 0;
-			break;
-		case 1:
-			randnum = rand() % 10;
-			if (randnum < 4) {
-				text_layer_set_text(s_info_layer, "You won the proxy war! You gain power!");
-				stats[0] += (rand() % 71 + 10);
-				stats[4] += (rand() % 31 + 10);
-			} else if (randnum < 8) {
-				text_layer_set_text(s_info_layer, "They won the proxy war and gain power.");
-				stats[2] += (rand() % 71 + 10);
-				stats[4] += (rand() % 31 + 10);
-			} else {
-				text_layer_set_text(s_info_layer, "The proxy country becomes independant.");
-				stats[4] -= (rand() % 31 + 10);
-			}
-			nonukestreak = 0;
-			break;
-		case 2:
-			randnum = rand() % 10;
-			if (randnum < 4) {
-				text_layer_set_text(s_info_layer, "You gain valuable intel about them.");
-				stats[2] += (rand() % 71 + 10);
-			} else if (randnum < 8) {
-				text_layer_set_text(s_info_layer, "Your spies are caught and they are angry.");
-				stats[4] += (rand() % 71 + 30);
-			} else {
-				text_layer_set_text(s_info_layer, "Your spies defect and tell them secrets.");
-				stats[3] += (rand() % 71 + 10);
-				stats[4] += (rand() % 31 + 10);
-			}
-			nonukestreak = 0;
-			break;
-		case 3:
-			randnum = rand() % 10;
-			if ((stats[4] > 300 || randnum > 7 ) || nonukestreak >= 3) {
-				text_layer_set_text(s_info_layer, "Your plea incites them to make more nukes.");
-				stats[1] += (rand() % 11 + 1);
-				stats[4] += (rand() % 21 + 10);
-			} else if (stats[4] > 150 || randnum > 2) {
-				text_layer_set_text(s_info_layer, "You both agree to cut down on the nukes.");
-				stats[0] -= (rand() % 11 + 5);
-				stats[1] -= (rand() % 11 + 5);
-				stats[4] -= (rand() % 41 + 20);
-			} else {
-				text_layer_set_text(s_info_layer, "They remove some nukes close to you.");
-				stats[1] -= (rand() % 11 + 1);
-				stats[4] -= (rand() % 21 + 10);
-			}
-			nonukestreak++;
-			break;
-	}
+	last_action = cell_index->row;
+	take_action(last_action);
+	update_stats_text();
+	window_stack_remove(s_menu_window, true);
 	post_turn_event();
 }
-
-
 
 static void timer_start_game(void *data) {
   window_stack_remove(s_initial_splash, false);
@@ -192,6 +235,8 @@ static void start_game_handler(ClickRecognizerRef recognizer, void *context) {
 	window_stack_push(s_main_window, false);
 }
 
+
+
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
 	if (end_game) {
 		window_stack_push(s_end_window, true);
@@ -200,12 +245,23 @@ static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
 	}
 }
 
+static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
+	if (!end_game && last_action) {
+		take_action(last_action);
+		update_stats_text();
+		post_turn_event();
+	}
+}
+
+
+
 static void click_to_start(void *context) {
 	window_single_click_subscribe(BUTTON_ID_SELECT, start_game_handler);
 };
 
 static void click_config_provider(void *context) {
 	window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+	window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
 };
 
 
